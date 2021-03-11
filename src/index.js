e218c0407003d387ec87b98c0adb70940ba1d2be
OKitel <kitelolga@gmail.com>
2021-03-11 15:13:50 +0100

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  let min = array[0] || 0;
+  for (i=1; i <= array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max (array = []) {
+  let max = array[0] || 0;
+  for (i=1; i <= array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+  }
+  return max;
+  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+  let result = 0;
+  for (i = 0; i <= array.length-1; i++) {
+    result += array[i];
+  }
+  if (array.length == 0) {
+    return result;
+  } else {
+    result = result/array.length;
+  }
+  return result;
 }
